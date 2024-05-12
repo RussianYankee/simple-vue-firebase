f0809985c2c754e06b8e342c93ed80b0495ed578
Anton Evlampeev <48838700+RussianYankee@users.noreply.github.com>
2024-05-12 00:36:45 -0500

    basic structure

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="main">
+    <label>Login</label>
+    <input :value="login" aria-label="www"/>
+    <label>Password</label>
+    <input :value="password" aria-label="www"/>
+    <button @click="click">Sign up</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import {ref} from "vue";
+
+const login = ref('')
+const password = ref('')
+
+const click = () => {
+  console.log('CLICK!')
+}
+</script>
+
+<style scoped>
+.main {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-items: center;
+}
+</style>
